ffe94e0769e38aee901916310a257a91b3110c23
Lucas Barés <lucasbaresm@gmail.com>
2019-06-12 22:35:25 -0300

    Update login.js

--- login.js
+++ login.js
@@ -64,7 +64,6 @@ dispatcher.add('../commands/ship.js')
 dispatcher.add('../commands/trello.js')
 dispatcher.add('../commands/mute.js')
 dispatcher.add('../commands/seppuku.js')
-dispatcher.add('../commands/waifu.js')
 dispatcher.register()
 console.timeEnd('dispatcher')
 
